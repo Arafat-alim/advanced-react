c7ed61c73e6c8f51fcd88ddcd7f84ef8d1aca7fc
Arafat Alim <Arafat97alim@gmail.com>
2023-01-06 18:35:11 +0530

    New Js Features

--- optimised/src/classbased/NewJsFeatures.jsx
+++ optimised/src/classbased/NewJsFeatures.jsx
@@ -1,37 +1,33 @@
 import React, { Component } from "react";
 
 class NewJsFeatures extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-    };
-    //! binding the normal function
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-  }
+  //! defining the state
+  state = {
+    count: 0,
+    greeting: "Hi",
+    age: 42,
+  };
 
   //! methods
-  increment() {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count + 1,
-      };
-    });
-  }
+  increment = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
 
-  decrement() {
+  decrement = () => {
     this.setState((prevState) => {
       return {
         count: prevState.count - 1,
       };
     });
-  }
+  };
   render() {
+    const { count: num, greeting, age } = this.state;
     return (
       <div>
         <h1>Counter Application</h1>
-        <h2>{this.state.count}</h2>
+        <h2>
+          {num} {greeting} {age}
+        </h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
       </div>
