9348bd7092da37ad040ee5fe59dcab6a28c61191
Arafat Alim <Arafat97alim@gmail.com>
2023-01-07 02:18:27 +0530

    Simple HOC

--- performance/src/App.jsx
+++ performance/src/App.jsx
@@ -1,19 +1,11 @@
-import CTA from "./Children/CTA";
+import { withPointlessHOC } from "./HOC/withPointlessHOC";
 
 const App = () => {
   return (
     <div className="app">
       <h1>React Application</h1>
-      <CTA>
-        <h1>HI I am the children props</h1>
-        <button>Click Me!</button>
-      </CTA>
-      <CTA>
-        <h2>I am the second children Props</h2>
-        <h3>You will love me! 💙</h3>
-      </CTA>
     </div>
   );
 };
 
-export default App;
+export default withPointlessHOC(App);
