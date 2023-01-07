37bcd853bf495c41ded62f3261a9638774939419
Arafat Alim <Arafat97alim@gmail.com>
2023-01-08 01:09:50 +0530

    HOC withToggle is completed and ready to go!

--- performance/src/HOC/withToggler.jsx
+++ performance/src/HOC/withToggler.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 
 //! created a class based component
+class Toggler extends Component {
+  //! state
+  state = {
+    show: false,
+  };
+  //! Methods
+  toggle = () => {
+    this.setState((prevState) => {
+      return {
+        show: !prevState.show,
+      };
+    });
+  };
+  render() {
+    const Component = this.props.component;
+    return <Component {...this.props} toggle={toggle} on={this.state.show} />;
+  }
+}
 
 export function withTOggler(component) {
   return function (props) {
