8bd774cc67f8ace9ba8af87d53cbad461f0eed26
Arafat Alim <Arafat97alim@gmail.com>
2023-01-14 17:28:26 +0530

    counting words is activate.

--- performance/src/hooks/speedTypingGame/GameApp.jsx
+++ performance/src/hooks/speedTypingGame/GameApp.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function GameApp() {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setText(value);
+  };
+  const handleSubmit = (text) => {
+    const wordSplit = text.trim().split(" ");
+    return wordSplit.filter((word) => word !== "").length;
+  };
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea />
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your Sentence"
+      />
       <h4>Time Remaining: 10</h4>
-      <button>Press Start</button>
-      <h1>Word Count: 10</h1>
+      <button onClick={handleSubmit}>Press Start</button>
+      <h1>Word Count: {handleSubmit(text)}</h1>
     </div>
   );
 }
