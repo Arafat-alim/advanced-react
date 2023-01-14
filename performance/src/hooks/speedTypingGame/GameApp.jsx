f95c440f15dc8f27cab7f154aeeb1e6ce2aafac0
Arafat Alim <Arafat97alim@gmail.com>
2023-01-14 17:38:38 +0530

    Created a timer

--- performance/src/hooks/speedTypingGame/GameApp.jsx
+++ performance/src/hooks/speedTypingGame/GameApp.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function GameApp() {
   const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
 
   const handleChange = (e) => {
     const { value } = e.target;
@@ -11,6 +12,15 @@ function GameApp() {
     const wordSplit = text.trim().split(" ");
     return wordSplit.filter((word) => word !== "").length;
   };
+
+  //! Useffect
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
   return (
     <div>
       <h1>How fast do you type?</h1>
@@ -19,7 +29,7 @@ function GameApp() {
         onChange={handleChange}
         placeholder="Enter your Sentence"
       />
-      <h4>Time Remaining: 10</h4>
+      <h4>Time Remaining: {timeRemaining}</h4>
       <button onClick={handleSubmit}>Press Start</button>
       <h1>Word Count: {handleSubmit(text)}</h1>
     </div>
