2ca02da39bacc263690d605d1bf01854f1595ab4
Arafat Alim <Arafat97alim@gmail.com>
2023-01-14 21:10:41 +0530

    Major features added.

--- performance/src/hooks/speedTypingGame/GameApp.jsx
+++ performance/src/hooks/speedTypingGame/GameApp.jsx
@@ -1,17 +1,37 @@
 import React, { useEffect, useState } from "react";
 
 function GameApp() {
+  const STARTING_COUNT = 5;
   const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_COUNT);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [countWord, setCountWord] = useState(0);
+  const [isDisanledBtn, setIsDisabledBtn] = useState(false);
+  const [isDisanledArea, setIsDisabledArea] = useState(true);
 
   const handleChange = (e) => {
     const { value } = e.target;
     setText(value);
   };
+
   const counting = (text) => {
     const wordSplit = text.trim().split(" ");
     return wordSplit.filter((word) => word !== "").length;
+  };
+
+  const startGame = () => {
+    setIsTimeRunning(true);
+    setTimeRemaining(STARTING_COUNT);
+    setIsDisabledBtn(true);
+    setIsDisabledArea(false);
+    setCountWord(0);
+    setText("");
+  };
+  const endGame = () => {
+    setIsTimeRunning(false);
+    setCountWord(counting(text));
+    setIsDisabledArea(true);
+    setIsDisabledBtn(false);
   };
 
   //! Useffect
@@ -20,21 +40,26 @@ function GameApp() {
       setTimeout(() => {
         setTimeRemaining((prevTime) => prevTime - 1);
       }, 1000);
-    } else if (isTimeRunning === 0) {
-      setIsTimeRunning(false);
+    } else if (timeRemaining === 0) {
+      endGame();
     }
   }, [timeRemaining, isTimeRunning]);
+
+  console.log(countWord);
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea
         value={text}
+        disabled={isDisanledArea}
         onChange={handleChange}
         placeholder="Enter your Sentence"
       />
       <h4>Time Remaining: {timeRemaining}</h4>
-      <button onClick={() => setIsTimeRunning(true)}>Press Start</button>
-      <h1>Word Count: {counting(text)}</h1>
+      <button onClick={startGame} disabled={isDisanledBtn}>
+        Start
+      </button>
+      <h1>Word Count: {countWord}</h1>
     </div>
   );
 }
