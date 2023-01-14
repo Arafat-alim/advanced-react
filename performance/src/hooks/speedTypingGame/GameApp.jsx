c7aadfd4ce821bd3eb35880378436602faa8a523
Arafat Alim <Arafat97alim@gmail.com>
2023-01-14 21:15:14 +0530

    App optimised.

--- performance/src/hooks/speedTypingGame/GameApp.jsx
+++ performance/src/hooks/speedTypingGame/GameApp.jsx
@@ -6,8 +6,6 @@ function GameApp() {
   const [timeRemaining, setTimeRemaining] = useState(STARTING_COUNT);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
   const [countWord, setCountWord] = useState(0);
-  const [isDisanledBtn, setIsDisabledBtn] = useState(false);
-  const [isDisanledArea, setIsDisabledArea] = useState(true);
 
   const handleChange = (e) => {
     const { value } = e.target;
@@ -22,16 +20,13 @@ function GameApp() {
   const startGame = () => {
     setIsTimeRunning(true);
     setTimeRemaining(STARTING_COUNT);
-    setIsDisabledBtn(true);
-    setIsDisabledArea(false);
+
     setCountWord(0);
     setText("");
   };
   const endGame = () => {
     setIsTimeRunning(false);
     setCountWord(counting(text));
-    setIsDisabledArea(true);
-    setIsDisabledBtn(false);
   };
 
   //! Useffect
@@ -45,18 +40,17 @@ function GameApp() {
     }
   }, [timeRemaining, isTimeRunning]);
 
-  console.log(countWord);
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea
         value={text}
-        disabled={isDisanledArea}
+        disabled={!isTimeRunning}
         onChange={handleChange}
         placeholder="Enter your Sentence"
       />
       <h4>Time Remaining: {timeRemaining}</h4>
-      <button onClick={startGame} disabled={isDisanledBtn}>
+      <button onClick={startGame} disabled={isTimeRunning}>
         Start
       </button>
       <h1>Word Count: {countWord}</h1>
