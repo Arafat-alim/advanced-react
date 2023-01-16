9589495cd25a00b60a1bcf6d7d3ebd85a1f8f36e
Arafat Alim <Arafat97alim@gmail.com>
2023-01-17 01:13:56 +0530

    React LInk

--- performance/src/React-Router-DOM/RouterApp.jsx
+++ performance/src/React-Router-DOM/RouterApp.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function RouterApp() {
-  return <div>RouterApp</div>;
+  return (
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/about"}>About</Link>
+    </div>
+  );
 }
 
 export default RouterApp;
