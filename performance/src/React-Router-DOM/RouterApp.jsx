50871f2bb567cadfa1d2b5e04348f95c06329d13
Arafat Alim <Arafat97alim@gmail.com>
2023-01-17 01:29:50 +0530

    Route, Swtich, component

--- performance/src/React-Router-DOM/RouterApp.jsx
+++ performance/src/React-Router-DOM/RouterApp.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Switch, Route } from "react-router-dom";
+
+import RouterHome from "./components/RouterHome";
+import RouterAbout from "./components/RouterAbout";
 
 function RouterApp() {
   return (
     <div>
       <Link to={"/"}>Home</Link>
       <Link to={"/about"}>About</Link>
+
+      <Switch>
+        {/* <Route exact path={"/"} render={() => <h1>Home</h1>} /> */}
+        <Route exact path="/">
+          <RouterHome />
+        </Route>
+        <Route path="/about">
+          <RouterAbout />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ performance/src/React-Router-DOM/components/RouterHome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RouterHome() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default RouterHome;
